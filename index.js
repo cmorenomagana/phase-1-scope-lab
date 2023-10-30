b9203a87530a8c7e5d256880ef493f1a82244923
Cuitlahuatl <cmorenomagana@gmail.com>
2023-10-30 16:38:12 -0500

    Scope Lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your solution in this file!
+customerName = `bob`
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+    return bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'doug'
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'bob'
+}
